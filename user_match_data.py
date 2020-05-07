108a9d020ed7524b9cfd44a9cfd59516b4b03d0f
Eric Zimmer <eric.zimmer@mymail.champlain.edu>
2020-05-07 19:08:57 -0400

    Fixed up formatting

--- user_match_data.py
+++ user_match_data.py
@@ -8,35 +8,46 @@ watcher = LolWatcher(api_key)
 region = "na1"
 
 
-# Get target summoner's username
-user = input("Please enter a summoner name: ")
+def get_match_data(account_id, user):
+    # Acquire match data
+
+    matchlist = watcher.match.matchlist_by_account(region, account_id) # Get their match history via account_id
+    match_id = matchlist["matches"][0]["gameId"] # Grab this user's most recent match_id
+    match_data = watcher.match.by_id(region, match_id) # Get match data from this match
 
 
-# Acquire match data
-account_id = watcher.summoner.by_name(region, user)["accountId"] # Get account_id of target user via their summoner name
-matchlist = watcher.match.matchlist_by_account(region, account_id) # Get their match history via account_id
-match_id = matchlist["matches"][0]["gameId"] # Grab this user's most recent match_id
-match_data = watcher.match.by_id(region, match_id) # Get match data from this match
+    # Filter down data from returned JSON file, towards search of a specific player
+    participants = match_data["participants"]
+    participantIDs = match_data["participantIdentities"]
 
 
-# Filter down data from returned JSON file, towards search of a specific player
-participants = match_data["participants"]
-participantIDs = match_data["participantIdentities"]
+    # Get individual user data for a match
+    for participant in participantIDs:  # Find Logic's participant ID
+        if participant["player"]["summonerName"] == user:
+            pid = int(participant["participantId"])
+            for participant in participants:    # Select Logic's data
+                if participant["participantId"] == pid:
+                    output = json.dumps(participant, indent=4)
+                    print(output) # Output result to console
+
+                    # Output result to a file
+                    # with open('out.txt', 'w') as outfile:
+                    # json.dump(output, outfile, indent=4)
 
 
-# Get individual user data for a match
-for participant in participantIDs:  # Find Logic's participant ID
-    if participant["player"]["summonerName"] == user:
-        pid = int(participant["participantId"])
-        for participant in participants:    # Select Logic's data
-            if participant["participantId"] == pid:
-                output = participant
+def main():
+    user = input("Please enter a summoner name: ") # Get target summoner's username
 
 
-# Output result to console
-output = json.dumps(output, indent=4)
-print(output)
+    try:
+        account_id = watcher.summoner.by_name(region, user)["accountId"]  # Get account_id of target user via their summoner name
 
-# Output result to a file
-#with open('out.txt', 'w') as outfile:
-    #json.dump(output, outfile, indent=4)
+    except ApiError as err:
+        if err.response.status_code == 404:
+            print('Summoner with that name not found.')
+    else:
+        get_match_data(account_id, user)
+
+main()
+
+
