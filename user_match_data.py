7462582445037f65bfc208184438eece9d2e25bc
Eric Zimmer <eric.zimmer@mymail.champlain.edu>
2020-05-08 19:56:44 -0400

    Added function to collect desired statistics from target game. Cleaned up other functions.

--- user_match_data.py
+++ user_match_data.py
@@ -3,13 +3,15 @@ from riotwatcher import LolWatcher, ApiError
 import json
 
 # Global Variables
-api_key = "RGAPI-39b4dca4-5490-4e15-a5f7-d7467e921d3f"
+api_key = "RGAPI-f46f08bc-505d-4817-b3ae-1bee84b8cf33"
 watcher = LolWatcher(api_key)
 region = "na1"
 
 
-def get_stats(match_data, player_data):
-    #  Player
+def get_stats(match_data, player_data, champlist):
+    # Tracked information:
+
+    # Player
         # Team (100, 200) || ["teamId"] -> 100 == Blue, 200 == Red
         # Win or Loss || ["stats"]["win"] -> true or false
         # Champion || ["championId"]
@@ -22,12 +24,92 @@ def get_stats(match_data, player_data):
         # Gold || ["stats"]["goldEarned"]
         # Gold per minute (Gold / Game length in minutes) || (Gold / int(fullmatchdata["gameDuration"] / 60))
         # Kill Participation || ((Kills + Assists) / Sum Team Kills)
-        # Shared Kills || (Kills / Sum Team Kills)
         # Team Gold Percentage || (Gold / Sum Team Gold)
 
-        # Work In Progress
+    # Convert JSON string back into JSON object
+    match_data, player_data = json.loads(match_data), json.loads(player_data)
 
+    # Data to be referenced
+    statistics = {"players" : []} # Store all game statistics
+    name_to_participant_id = {} # Mapping of player names to participant ids
+    participant_id_to_name = {} # Mapping of participant ids to player names
 
+    # Team-wide sum values to be used for calculations later
+    blue_team_kills = 0
+    blue_team_gold = 0
+    red_team_kills = 0
+    red_team_gold = 0
+
+    # Enter the name each player into the data set
+    for player in match_data["participantIdentities"]:
+        statistics["players"].append({"player" : player["player"]["summonerName"]})
+        name_to_participant_id[player["player"]["summonerName"]] = player["participantId"] # Map player name to participant id
+        participant_id_to_name[str(player["participantId"])] = player["player"]["summonerName"] # Map participant ids to player names
+
+    # Cycle through each player entering their respective data
+    for participant in range(len(match_data["participants"])):
+
+        # Team Color
+        if match_data["participants"][participant]["teamId"] == 100:
+            statistics["players"][participant]["side"] = "blue"
+        elif match_data["participants"][participant]["teamId"] == 200:
+            statistics["players"][participant]["side"] = "red"
+
+        # Win or Loss
+        if match_data["participants"][participant]["stats"]["win"] == True:
+            statistics["players"][participant]["win"] = True
+        elif match_data["participants"][participant]["stats"]["win"] == False:
+            statistics["players"][participant]["win"] = False
+
+        # Champion
+        statistics["players"][participant]["champion"] = get_champ_from_id(champlist, match_data["participants"][participant]["championId"])
+
+        # Combat Stats
+        statistics["players"][participant]["kills"] = match_data["participants"][participant]["stats"]["kills"]
+
+        # Increment team kill counts based on which team the player represents
+        if statistics["players"][participant]["side"] == "blue":
+            blue_team_kills += match_data["participants"][participant]["stats"]["kills"]
+        else:
+            red_team_kills += match_data["participants"][participant]["stats"]["kills"]
+
+        statistics["players"][participant]["deaths"] = match_data["participants"][participant]["stats"]["deaths"]
+        statistics["players"][participant]["assists"] = match_data["participants"][participant]["stats"]["assists"]
+
+        # KDA = (Kills + Assists) / Deaths
+        statistics["players"][participant]["kda"] = round((statistics["players"][participant]["kills"] + statistics["players"][participant]["assists"]) / statistics["players"][participant]["deaths"], 2)
+
+        # Farming Statistics
+        statistics["players"][participant]["creepscore"] = (match_data["participants"][participant]["stats"]["totalMinionsKilled"] + match_data["participants"][participant]["stats"]["neutralMinionsKilled"])
+        statistics["players"][participant]["creepspermin"] = round(statistics["players"][participant]["creepscore"] / int(match_data["gameDuration"] / 60), 1)
+
+        # Gold Statistics
+        statistics["players"][participant]["gold"] = match_data["participants"][participant]["stats"]["goldEarned"]
+
+        # Increment team gold counts based on which team the player represents
+        if statistics["players"][participant]["side"] == "blue":
+            blue_team_gold += match_data["participants"][participant]["stats"]["goldEarned"]
+        else:
+            red_team_gold += match_data["participants"][participant]["stats"]["goldEarned"]
+
+        statistics["players"][participant]["goldpermin"] = round(statistics["players"][participant]["gold"] / int(match_data["gameDuration"] / 60), 1)
+
+    # Cycle back through to append team-wide data
+    for participant in range(len(match_data["participants"])):
+
+        # Kill Participation based on team || (Kills + Assists) / Team Kills
+        if statistics["players"][participant]["side"] == "blue":
+            statistics["players"][participant]["killparticipationpercent"] = round((statistics["players"][participant]["kills"] + statistics["players"][participant]["assists"]) / blue_team_kills, 2)
+        else:
+            statistics["players"][participant]["killparticipationpercent"] = round((statistics["players"][participant]["kills"] + statistics["players"][participant]["assists"]) / red_team_kills, 2)
+
+        # Team Gold Percentage based on team || Gold / Team Gold
+        if statistics["players"][participant]["side"] == "blue":
+            statistics["players"][participant]["teamgoldpercent"] = round(statistics["players"][participant]["gold"] / blue_team_gold, 2)
+        else:
+            statistics["players"][participant]["teamgoldpercent"] = round(statistics["players"][participant]["gold"] / red_team_gold, 2)
+
+    return statistics
 
 
 def get_match_data(account_id, user):
@@ -37,11 +119,9 @@ def get_match_data(account_id, user):
     match_id = matchlist["matches"][0]["gameId"] # Grab this user's most recent match_id
     match_data = watcher.match.by_id(region, match_id) # Get match data from this match
 
-
     # Filter down data from returned JSON file, towards search of a specific player
     participants = match_data["participants"]
     participantIDs = match_data["participantIdentities"]
-
 
     # Get individual user data for a match
     for participant in participantIDs:  # Find Logic's participant ID
@@ -52,24 +132,19 @@ def get_match_data(account_id, user):
                     output = json.dumps(participant, indent=4)
                     return json.dumps(match_data), json.dumps(output)
 
-
                     # Output result to a file
                     # with open('out.txt', 'w') as outfile:
                         # json.dump(participant, outfile, indent=4)
 
 
-def get_champ_from_id(id):
-    # Collect champion_id
-    id = input("Please enter a champion id: ")
+def get_champ_from_id(champlist, id):
 
     # Acquire champion data
-    version = watcher.data_dragon.versions_for_region(region)["v"]
-    champlist = watcher.data_dragon.champions(version)["data"]
     champions = champlist.keys()
 
     # Identify the right champion and print its name
     for champ in champions:
-        if champlist[champ]["key"] == id:
+        if champlist[champ]["key"] == str(id):
             return champ
 
 
@@ -78,14 +153,17 @@ def main():
 
     try:
         account_id = watcher.summoner.by_name(region, user)["accountId"]  # Get account_id of target user via their summoner name
-
     except ApiError as err:
         if err.response.status_code == 404:
             print('Summoner with that name not found.')
     else:
-        match_data, player_data = get_match_data(account_id, user)
-        get_stats(match_data, player_data)
+        # API Calls to collect data
+        match_data, player_data = get_match_data(account_id, user) # Get match data
+        version = watcher.data_dragon.versions_for_region(region)["v"]  # Get game version
+        champlist = watcher.data_dragon.champions(version)["data"]  # Get champion data
+
+        statistics = get_stats(match_data, player_data, champlist) # Produce statistics from the game
+        print(statistics)
+
 
 main()
-
-
